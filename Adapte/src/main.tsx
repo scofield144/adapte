674aaccacf7efcfcfc31fca93a716a7f2834014d
scofield <talesscof@gmail.com>
2024-09-15 18:17:20 +0000

    add logo

--- Adapte/src/main.tsx
+++ Adapte/src/main.tsx
@@ -19,16 +19,9 @@ const router = createBrowserRouter([
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
+
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 )
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );

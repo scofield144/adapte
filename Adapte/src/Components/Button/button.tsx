bda37148334642a303c805102cf43c79079c593b
scofield <talesscof@gmail.com>
2024-09-15 18:30:58 +0000

    routine

--- Adapte/src/Components/Button/button.tsx
+++ Adapte/src/Components/Button/button.tsx
@@ -1,21 +1,25 @@
 import { Link } from "react-router-dom";
-interface url {
+interface ButtonProps {
   address: string;
   head?: string;
+  label?: string;
 }
 
-const Button: React.FC<url> = ({ address }) => {
+const Button: React.FC<ButtonProps> = ({ address, label = "Saber Mais" }) => {
   return (
-    <button
+    
+    <Link
+      to={address}
       type="button"
-      className="bg-Black 
-      text-white font-semibold py-3 px-6 rounded-lg max-w-fit
+      className="bg-Black text-white font-semibold py-3 px-6 rounded-lg max-w-fit
       hover:bg-white hover:text-Black hover:border-Black transition-all duration-500 ease-in-out
-      hover:border-solid hover:border-2"
-      
+      hover:border-solid hover:border-2 inline-block text-center"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="label"
     >
-      <Link to={address}>Saber Mais</Link>
-    </button>
+      {label}
+    </Link>
   );
 };
 
--- Adapte/src/pages/Projects/Project.tsx
+++ Adapte/src/pages/Projects/Project.tsx
@@ -6,48 +6,52 @@ const projects = [
     title: "Mentoria",
     content:
       "Um programa para apoiar jovens do ensino médio no desenvolvimento pessoal e acadêmico por meio de mentoria",
-    link: "#",
+    link: "https://www.facebook.com/profile.php?id=61564223186228",
   },
   {
     id: 2,
     title: "WhatCV",
     content:
       "Um serviço que permite aos usuários gerar currículos de forma rápida e simples através de mensagens no WhatsApp",
-    link: "#",
+    link: "https://www.facebook.com/profile.php?id=61564223186228",
   },
   {
     id: 3,
     title: "ESM",
     content:
       "Um aplicativo que conecta pacientes com médicos para consultas domiciliares e cuidados de saúde personalizados.",
-    link: "#",
+    link: "https://www.facebook.com/profile.php?id=61564223186228",
   },
   {
     id: 4,
     title: "AJH",
     content:
       "Uma plataforma online que conecta voluntários e doadores a iniciativas de ajuda humanitária em Angola, especialmente em resposta a desastres naturais e outras crises",
-    link: "#",
+    link: "https://www.facebook.com/profile.php?id=61564223186228",
   },
 ];
 //h-[558.46px]
 const Project: React.FC = () => {
   return (
-    <div id="project"
+    <section id="project"
       className="flex 
     flex-col justify-around h-full pb-10
     items-center gap-6  pt-10 md-min:mt-10 min:mt-10 md-min:rounded-lg
     md-min:bg-white
     "
     >
-      <h2 className=" text-3xl text-left font-bold">NOSSOS PROJETOS</h2>
+      <header className=" text-3xl text-left font-bold">NOSSOS PROJETOS</header>
       <div className="  grid grid-cols-1   tablet:grid-cols-2 
       laptop:grid-cols-3 desktop:grid-cols-4 gap-4 ">
         {projects.map((project) => (
-          <Card key={project.id} title={project.title} content={project.content} link={project.link}/>
+          <Card 
+          key={project.id}
+          title={project.title} 
+          content={project.content} 
+          link={project.link}/>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
